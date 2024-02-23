0f89ad28a0c2a2aae0de337cc5f83c071ce1562b
jonlar93 <127976348+jonlar93@users.noreply.github.com>
2024-02-23 13:54:55 +0100

    fixed images
    fixed images
    added non visual alt text

--- js/events/navigation.js
+++ js/events/navigation.js
@@ -1,16 +1,12 @@
 export function toggleNav() {
-  const nav = document.getElementById("navTop"); // Access the first `nav` element in the document.
+  const nav = document.getElementById("navTop");
 
   nav.style.display = nav.style.display === "block" ? "none" : "block";
 }
 
-/**
- * Sets up a click event listener on the hamburger button to toggle the navigation menu.
- */
 export function hamburgerButton() {
-  const hamburger = document.getElementById("hamburger"); // Get the hamburger button element by its ID.
+  const hamburger = document.getElementById("hamburger");
 
-  // Add an event listener to call `toggleNav` when the button is clicked.
   hamburger.addEventListener("click", toggleNav); // Pass `toggleNav` directly.
 }
 
